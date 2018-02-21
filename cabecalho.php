94ddca700e3fb4335b8d75121c49439cec9f4dff
Roberto Vinícius <developer.roberto@gmail.com>
2018-02-21 20:08:12 -0300

    Substituindo o link livros na navbar por um dropdown com os links para os formularios relacionados a livro

--- cabecalho.php
+++ cabecalho.php
@@ -24,23 +24,18 @@ require_once("conecta.php");
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item <?= basename($_SERVER['PHP_SELF']) == "lista-livros.php" ? "active" : "" ?>">
-        <a class="nav-link" href="lista-livros.php">Livros</a>
-      </li>
-      <li class="nav-item <?= basename($_SERVER['PHP_SELF']) == "cadastro-livro.php" ? "active" : "" ?>">
-        <a class="nav-link" href="cadastro-livro.php">Cadastro</a>
-      </li>
+
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+        <a class="nav-link dropdown-toggle <?= basename($_SERVER['PHP_SELF']) == "lista-livros.php" ? "active" : "" ?>" href="lista-livros.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Livros
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="lista-livros.php">Lista</a>
+          <a class="dropdown-item" href="cadastro-livro.php">Cadastro</a>
         </div>
       </li>
+
+
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Nome/ISBN" aria-label="Search">
