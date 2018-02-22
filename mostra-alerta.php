b9bd4d59dc32158711d688c09df1334a1354ca1b
Roberto Vinícius <developer.roberto@gmail.com>
2018-02-22 05:54:55 -0300

    adicionado a classe alert-dismissable no alert, habilitando a opcao de descartar o alerta

--- mostra-alerta.php
+++ mostra-alerta.php
@@ -3,7 +3,14 @@ session_start();
  function mostraAlerta($tipo) {
      if(isset($_SESSION[$tipo])) {
 ?>
-    <div class="alert alert-<?= $tipo ?>"><?= $_SESSION[$tipo]?></div>
+
+	<div class="alert alert-<?= $tipo ?> alert-dismissible fade show" role="alert">
+  	<?= $_SESSION[$tipo]?>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
 <?php
         unset($_SESSION[$tipo]);
      }
