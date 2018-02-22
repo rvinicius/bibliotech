28d23340c6dbdd0fa84659b293e85c2b7d4f5138
Roberto Vinícius <developer.roberto@gmail.com>
2018-02-22 04:25:28 -0300

    adicionado arquivo com o formulario de alteracao de um livro

---
+++ altera-livro.php
@@ -0,0 +1,31 @@
+<?php 
+	require_once('cabecalho.php');
+
+	$livroDao = new LivroDAO($conexao);
+	$livro = $livroDao->buscaLivro($_GET['id']);
+
+ ?>
+
+<br/>
+<div class="container card">
+ 	<h5 class="card-title">Alteracao de livro</h5>
+<form action="update-livro.php" method="post">
+  <div class="form-row card-body ">
+
+<input type="hidden" name="id" value="<?= $livro->getId();?>">
+    <div class="form-group col-md-5">
+      <label for="inputEmail4">Nome</label>
+      <input type="text" class="form-control" name="nome" value="<?= $livro->getNome(); ?>">
+    </div>
+    <div class="form-group col-md-5">
+      <label for="ISBN">ISBN</label>
+      <input type="text" class="form-control" name="isbn" value="<?= $livro->getIsbn(); ?>">
+    </div>
+
+     <div class="form-group col-md-10">
+     	 <input type="submit" value="Alterar" class="btn btn-primary float-right" />
+     </div>
+  </div>
+
+</form>
+</div>
