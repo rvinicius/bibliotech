182b0d4bb42cc7595580a65befafe5f4f83dc37c
Roberto Vinícius <developer.roberto@gmail.com>
2018-02-22 04:52:07 -0300

    alteracao na nomenclatura usada na tabela para descrever um livro

--- lista-livros.php
+++ lista-livros.php
@@ -5,24 +5,36 @@ require_once("class/LivroDAO.php");
 $livroDao = new LivroDAO($conexao);
 $livros = $livroDao->listaLivros();
 
+
+
 ?>
-	<div class="container-fluid">
-		<table class="table table-bordered">
+
+<?php mostraAlerta("success");
+      mostraAlerta("danger");
+ ?>
+	<div class="container">
+		<table class="table table-bordered ">
 	  <thead class="thead-light">
 	    <tr>
-	      <th scope="col">#</th>
-	      <th scope="col">Nome</th>
+	      <th scope="col">Cod.</th>
+	      <th scope="col">Titulo</th>
 	      <th scope="col">ISBN</th>
 	      <th scope="col"></th>
+	       <th scope="col"></th>
 	    </tr>
 	  </thead>
 	  <tbody>
 	  	<?php foreach ($livros as $livro) : ?>
 	    <tr>
-	      <th scope="row"><?= $livro->getId(); ?></th>
-	      <td><?= $livro->getNome(); ?></td>
-	      <td><?= $livro->getIsbn(); ?></td>
-	      <td><a href="altera-livro.php?id=<?= $livro->getId(); ?>" class="btn btn-outline-primary ">Alterar</a></td>
+	        <th scope="row"><?= $livro->getId(); ?></th>
+	        <td><?= $livro->getNome(); ?></td>
+	        <td><?= $livro->getIsbn(); ?></td>
+	        <td class="text-center"><a href="altera-livro.php?id=<?= $livro->getId(); ?>" class="btn btn-outline-primary ">Alterar</a></td>
+  
+	        <form action="remove-livro.php" method="post">
+	        	<input type="hidden" name="id" value="<?= $livro->getId() ?>">
+	            <td class="text-center"><button class="btn btn-outline-danger ">Excluir</a></td>
+	  	</form>
 	    </tr>
 	<?php endforeach ?>
 	  </tbody>
