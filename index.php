9fba1c94f90e3b00cbe106f61d1bf22e81ed02e3
Roberto Vinícius <developer.roberto@gmail.com>
2018-02-28 01:18:25 -0300

    Design do formulario de autenticacao alterado.

--- index.php
+++ index.php
@@ -10,21 +10,25 @@
 		<p class="text-success">Você está logado como <?= usuarioLogado(); ?></p>
         <a href="logout.php">Deslogar</a></p>
 <?php } else { ?>
-	<h1>Login</h1>
-    <form action="login.php" method="post">
-        <table class="table">
-            <tr>
-                <td>Login</td>
-                <td><input class="form-control" type="text" name="login"></td>
-            </tr>
-            <tr>
-                <td>Senha</td>
-                <td><input class="form-control" type="password" name="senha"></td>
-            </tr>
-            <tr>
-                <td><button type="submit" class="btn btn-primary">Login</button></td>
-            </tr>
-        </table>
+<div class="container card col-md-3 mt-3 ">
+  <h2 class="card-title pt-2">Login</h2>
+<form action="login.php" method="post">
+  <div class="card-body ">
+    <div class="form-group ">
+        <label for="login">Login</label>
+        <input class="form-control" type="text" name="login">
+    </div>
+    <div class="form-group">
+        <label for="senha">Senha</label>
+        <input class="form-control" type="password" name="senha">
+    </div>
+
+    <div class="form-group d-flex flex-row-reverse">
+       <input type="submit" value="Login" class="btn btn-primary float-right" />
+     </div>
+  </div>
+
+
     </form>
 	 <?php }?>
 <?php include("rodape.php"); ?>
