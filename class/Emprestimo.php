422bc07cb746071b9538e2326e30082a60631493
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-03 03:04:46 -0300

    Alterado o campo id_usuario e id_livro para usuario e livro, respectivamente. O construtor passou a receber um objeto do tipo Usuario e Livro. Metodos alterados para CamelCase.

--- class/Emprestimo.php
+++ class/Emprestimo.php
@@ -3,17 +3,17 @@
 class Emprestimo
 {
     private $id;
-    private $id_usuario;
-    private $id_livro;
+    private $usuario;
+    private $livro;
     private $data_emprestimo;
     private $data_entrega;
     private $multa;
     private $status; 
 
-    function __construct($id_usuario, $id_livro, $data_emprestimo, $data_entrega, $multa, $status )
+    function __construct(Usuario $usuario, Livro $livro, $data_emprestimo, $data_entrega, $multa, $status)
     {
-        $this->id_usuario = $id_usuario;
-        $this->id_livro = $id_livro;
+        $this->usuario = $usuario;
+        $this->livro = $livro;
         $this->data_emprestimo = $data_emprestimo;
         $this->data_entrega = $data_entrega;
         $this->multa = $multa;
@@ -30,42 +30,42 @@ class Emprestimo
     	$this->id = $id;
     }
 
-	public function getId_usuario()
+	public function getUsuario()
     {
-        return $this->id_usuario;
+        return $this->usuario;
     }
 
-    public function setId_usuario($id_usuario)
+    public function setUsuario($usuario)
     {
-        $this->id_usuario = $id_usuario;
+        $this->usuario = $usuario;
     }
 
-    public function getId_livro()
+    public function getLivro()
     {
-        return $this->id_livro;
+        return $this->livro;
     }
 
-    public function setId_livro($id_livro)
+    public function setLivro($livro)
     {
-    	$this->id_livro = $id_livro;
+    	$this->livro = $livro;
     }
 
-    public function getData_emprestimo()
+    public function getDataEmprestimo()
     {
         return $this->data_emprestimo;
     }
 
-    public function setData_emprestimo($data_emprestimo)
+    public function setDataEmprestimo($data_emprestimo)
     {
     	$this->data_emprestimo = $data_emprestimo;
     }
 
-    public function getData_entrega()
+    public function getDataEntrega()
     {
         return $this->data_entrega;
     }
 
-    public function setData_entrega($data_entrega)
+    public function setDataEntrega($data_entrega)
     {
     	$this->data_entrega = $data_entrega;
     }
