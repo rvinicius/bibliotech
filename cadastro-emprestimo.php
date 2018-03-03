b036f7d6169a0d8c09e95700b2b66689b0c460dd
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-03 03:01:20 -0300

    Pequenos ajustes no layout do formulario

--- cadastro-emprestimo.php
+++ cadastro-emprestimo.php
@@ -12,33 +12,34 @@ $livros = $livrosDao->listaLivros();
 ?>
 
 <div class="container card">
-  <h2 class="card-title">Cadastro de empréstimo</h2>
-<form action="insert-emprestimo.php" method="post">
-  <div class="form-row card-body ">
-
-    <div class="form-group col-md-5">
-     <label for="usuario">Usuário</label>
-		<select name="usuario_id" class="form-control">
-            <?php foreach ($usuarios as $usuario) : ?>
-                <option value="<?= $usuario->getId() ?>"><?= $usuario->getId()." - ".$usuario->getNome() ?></option>
-            <?php endforeach ?>
-        </select>
-    </div>
+    <h2 class="card-title pt-3">Cadastro de empréstimo</h2>
+    <form action="insert-emprestimo.php" method="post">
+        <div class="form-row card-body">
     
-    <div class="form-group col-md-5">
-     <label for="livro">Livro</label>
-		<select name="usuario_id" class="form-control">
-            <?php foreach ($livros as $livro) : ?>
-                <option value="<?= $livro->getId() ?>"><?= $livro->getId()." - ".$livro->getNome() ?></option>
-            <?php endforeach ?>
-        </select>
-    </div>
-    
+            <div class="form-group col-md-6">
+                <label for="usuario">Usuário</label>
+                <select name="id_usuario" class="form-control">
+                    <?php foreach ($usuarios as $usuario) : ?>
+                        <option value="<?= $usuario->getId() ?>"><?= $usuario->getId()." - ".$usuario->getNome() ?></option>
+                    <?php endforeach ?>
+                </select>
+            </div>
+        
+            <div class="form-group col-md-6">
+                <label for="livro">Livro</label>
+                    <select name="id_livro" class="form-control">
+                    <?php foreach ($livros as $livro) : ?>
+                        <option value="<?= $livro->getId() ?>"><?= $livro->getId()." - ".$livro->getNome() ?></option>
+                    <?php endforeach ?>
+                </select>
+            </div>
 
-     <div class="form-group col-md-10">
-       <input type="submit" value="Cadastrar" class="btn btn-primary float-right" />
-     </div>
-  </div>
-
-</form>
+            <div class="form-group col-md-12">
+               <input type="submit" value="Gerar empréstimo" class="btn btn-primary float-right" />
+            </div>
+            <div class="alert alert-info col-md-12" role="alert">
+                O empréstimo tem duração de 7 dias úteis e é responsabilidade do dono do empréstimo fazer a renovação ou a entrega dentro da data. A multa para cada dia fora do prazo é de <strong>1 R$</strong>.
+            </div>
+        </div>
+    </form>
 </div>
