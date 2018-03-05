6e2449f355e2d8a148e5e5a025c585e22b6cb971
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-05 08:34:03 -0300

    Alteração no construtor de livro, alteração no metodo que atualiza um emprestimo para atualizar o status de um livro para indisponivel quando estiver relacionado a um emprestimo

--- class/EmprestimoDAO.php
+++ class/EmprestimoDAO.php
@@ -44,8 +44,9 @@ class EmprestimoDAO
 			$nome_livro = $emprestimo_array['nome_livro'];
 			$isbn = $emprestimo_array['isbn'];
 			$autor = $emprestimo_array['autor'];
+			$disponibilidade = $emprestimo_array['disponibilidade'];
 
-			$livro = new Livro($nome_livro, $isbn, $autor);
+			$livro = new Livro($nome_livro, $isbn, $autor, $disponibilidade);
 			$livro->setId($id_livro);
 
 			// $usuarioDao = new UsuarioDAO($this->conexao);
@@ -82,9 +83,12 @@ class EmprestimoDAO
         							   '{$emprestimo->getDataEmprestimo()}',
         							   '{$emprestimo->getDataEntrega()}',
         							   '{$emprestimo->getMulta()}',
-        							   '{$emprestimo->getStatus()}')";
-        
-		return mysqli_query($this->conexao, $query);
+        							   '{$emprestimo->getStatus()}');";
+        $update = "UPDATE livros SET disponibilidade='indisponivel' WHERE id='{$emprestimo->getLivro()->getId()}'";
+
+        $sql = $query.$update;
+
+		return mysqli_multi_query($this->conexao, $sql);
 	}
 
 	public function buscaEmprestimo($id)
@@ -114,7 +118,7 @@ class EmprestimoDAO
 
 	public function alteraEmprestimo(Emprestimo $emprestimo)
 	{
-		$query = "UPDATE emprestimo 
+		$query = "UPDATE emprestimos 
 						SET
 						id_usuario ='{$emprestimo->getUsuario()->getId()}',
 						id_livro ='{$emprestimo->getLivro()->getId()}',
@@ -134,6 +138,66 @@ class EmprestimoDAO
 
         return mysqli_query($this->conexao, $query);
     }
+
+
+	public function buscaPorStatus($status)
+	{
+	$emprestimos = array();
+	$resultado = mysqli_query($this->conexao, "
+					SELECT emprestimos.*, usuarios.*, usuarios.nome AS nome_usuario, livros.*, livros.nome AS nome_livro, emprestimos.id AS id_emprestimo
+					FROM emprestimos INNER JOIN usuarios ON emprestimos.id_usuario=usuarios.id
+					INNER JOIN livros ON emprestimos.id_livro=livros.id
+					WHERE status = '{$status}';");
+
+		while($emprestimo_array = mysqli_fetch_assoc($resultado)) {
+			$id = $emprestimo_array['id_emprestimo'];
+			$id_usuario = $emprestimo_array['id_usuario'];
+			$id_livro = $emprestimo_array['id_livro'];
+			$data_emprestimo = $emprestimo_array['data_emprestimo'];
+			$data_entrega = $emprestimo_array['data_entrega'];
+			$multa = $emprestimo_array['multa'];
+			$status = $emprestimo_array['status'];
+
+			//dados do usuario
+			$nome_usuario = $emprestimo_array['nome_usuario'];
+			$data_nascimento = $emprestimo_array['dataNascimento'];
+			$cpf = $emprestimo_array['cpf'];
+			$rg = $emprestimo_array['rg'];
+			$email = $emprestimo_array['email'];
+			$endereco = $emprestimo_array['endereco'];
+			$telefone = $emprestimo_array['telefone'];
+
+			$usuario = new Usuario($nome_usuario, $data_nascimento, $cpf, $rg, $email, $endereco, $telefone);
+			$usuario->setId($id_usuario);
+
+			//dados do livro
+			$nome_livro = $emprestimo_array['nome_livro'];
+			$isbn = $emprestimo_array['isbn'];
+			$autor = $emprestimo_array['autor'];
+			$disponibilidade = $emprestimo_array['disponibilidade'];
+
+			$livro = new Livro($nome_livro, $isbn, $autor, $disponibilidade);
+			$livro->setId($id_livro);
+
+			// $usuarioDao = new UsuarioDAO($this->conexao);
+			// $usuario = $usuarioDao->buscaUsuario($id_usuario);
+
+			// $livroDao = new LivroDAO($this->conexao);
+			// $livro = $livroDao->buscaLivro($id_livro);
+
+			$emprestimo = new Emprestimo($usuario,
+										 $livro,
+										 $data_emprestimo,
+										 $data_entrega,
+										 $multa,
+										 $status);
+
+			$emprestimo->setId($id);
+
+			array_push($emprestimos, $emprestimo);
+		}
+		return $emprestimos;
+	}
 
 }
 
