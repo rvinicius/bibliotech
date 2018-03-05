2de31e5f0a903fc8c33ed708bf4a21152a7fc1de
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-05 08:37:18 -0300

    Atributo disponibilidade adicionado as queries

--- class/LivroDAO.php
+++ class/LivroDAO.php
@@ -22,8 +22,9 @@ class LivroDAO
 			$id_livro = $livro_array['id'];
 			$isbn = $livro_array['isbn'];
 			$autor = $livro_array['autor'];
+			$disponibilidade = $livro_array['disponibilidade'];
 
-			$livro = new Livro($nome_livro, $isbn, $autor);
+			$livro = new Livro($nome_livro, $isbn, $autor, $disponibilidade);
 
 			$livro->setId($id_livro);
 
@@ -35,7 +36,7 @@ class LivroDAO
 
 	public function insereLivro(Livro $livro)
 	{
-		$query = "INSERT INTO livros(nome, isbn, autor) values('{$livro->getNome()}','{$livro->getIsbn()}', '{$livro->getAutor()}')";
+		$query = "INSERT INTO livros(nome, isbn, autor, disponibilidade) values('{$livro->getNome()}','{$livro->getIsbn()}', '{$livro->getAutor()}', '{$livro->getDisponibilidade()}')";
 
 		return mysqli_query($this->conexao, $query);
 	}
@@ -46,7 +47,7 @@ class LivroDAO
 		$resultado = mysqli_query($this->conexao, $query);
 		$livro_buscado = mysqli_fetch_assoc($resultado);
 
-		$livro = new Livro($livro_buscado['nome'], $livro_buscado['isbn'], $livro_buscado['autor']);
+		$livro = new Livro($livro_buscado['nome'], $livro_buscado['isbn'], $livro_buscado['autor'], $livro_buscado['disponibilidade']);	
 		$livro->setId($id);
 
 		return $livro;
@@ -54,7 +55,7 @@ class LivroDAO
 
 	public function alteraLivro(Livro $livro)
 	{
-		$query = "update livros set nome='{$livro->getNome()}', isbn='{$livro->getIsbn()}', autor='{$livro->getAutor()}' where id='{$livro->getId()}'";
+		$query = "update livros set nome='{$livro->getNome()}', isbn='{$livro->getIsbn()}', autor='{$livro->getAutor()}', disponibilidade='{$livro->getDisponibilidade()}' where id='{$livro->getId()}'";
 		var_dump($query);
 		return mysqli_query($this->conexao, $query);
 
@@ -78,8 +79,9 @@ class LivroDAO
 			$id_livro = $livro_array['id'];
 			$isbn = $livro_array['isbn'];
 			$autor = $livro_array['autor'];
+			$disponibilidade = $livro_array['disponibilidade'];
 
-			$livro = new Livro($nome_livro, $isbn, $autor);
+			$livro = new Livro($nome_livro, $isbn, $autor, $disponibilidade);
 
 			$livro->setId($id_livro);
 
