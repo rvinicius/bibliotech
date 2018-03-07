4c45d8f1aa54cd49949de050a8f7164c7684fde2
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-07 11:09:53 -0300

    Alterado o redirect quando o usuario consegue se logar no sistema

--- login.php
+++ login.php
@@ -5,12 +5,12 @@ require_once("logica-usuario.php");
 $usuario = buscaUsuario($conexao, $_POST["login"], $_POST["senha"]);
 
 if ($usuario == null) {
-	$_SESSION["danger"] = "Falha ao logar.";
-	header("Location: index.php");
+    $_SESSION["danger"] = "Falha ao logar.";
+    header("Location: index.php");
 }else{
-	$_SESSION["success"] = "Logado com sucesso.";
-	logaUsuario($usuario['login']);
-	header("Location: index.php");
+    $_SESSION["success"] = "Logado com sucesso.";
+    logaUsuario($usuario['login']);
+    header("Location: lista-emprestimos.php");
 }
 
 var_dump($usuario);
