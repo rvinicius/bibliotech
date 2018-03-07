821c671f8ac001bf468276261e4a7c3140ad2a66
Roberto Vinícius <developer.roberto@gmail.com>
2018-03-07 11:08:43 -0300

    Adicionado o if que verifica se já existe a sessão antes de criá-la

--- logica-usuario.php
+++ logica-usuario.php
@@ -1,28 +1,30 @@
 <?php 
 
-session_start();
+if(!isset($_SESSION)){
+    session_start();
+}
 
 function usuarioEstaLogado(){
-	return isset($_SESSION['usuario_logado']);
+    return isset($_SESSION['usuario_logado']);
 }
 
 function verificaUsuario(){
-	if (!usuarioEstaLogado()) {
-		$_SESSION["danger"]= "Você não tem acesso a essa informação.";
-		header("Location: index.php");
-    	die();
-	}
+    if (!usuarioEstaLogado() && basename($_SERVER['PHP_SELF']) != 'index.php') {
+        $_SESSION["danger"]= "Você não tem acesso a essa informação.";
+        header("Location: index.php");
+        die();
+    }
 }
 
 function usuarioLogado(){
-	return $_SESSION['usuario_logado'];
+    return $_SESSION['usuario_logado'];
 }
 
 function logaUsuario($login){
-	$_SESSION["usuario_logado"] = $login;
+    $_SESSION["usuario_logado"] = $login;
 }
 
 function logout(){
-	session_destroy();
-	session_start();	
+    session_destroy();
+    session_start();	
 }
